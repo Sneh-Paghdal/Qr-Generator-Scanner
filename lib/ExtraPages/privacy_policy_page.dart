a5a893a7f6988b7b93e7b89cb69c0fdb3225afbd
Sneh-Paghdal <paghadalsneh@gmail.com>
2023-06-17 18:41:27 +0530

    Privacy policy and terms pages added

--- lib/ExtraPages/privacy_policy_page.dart
+++ lib/ExtraPages/privacy_policy_page.dart
@@ -32,11 +32,103 @@ class _privacy_policy_pageState extends State<privacy_policy_page> {
         child: Column(
           children: [
             Container(
+              decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(10),
+                  color: constant.secondaryBackClr
+              ),
               margin: EdgeInsets.all(10),
               padding: EdgeInsets.all(10),
               child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
                 children: [
-                  Text("")
+                  Text(
+                    'Welcome to Jasneh! This Privacy Policy explains how we collect, use, and safeguard your personal information when you use our QR code scanning and QR code generator app. Please read this Privacy Policy carefully to understand our practices regarding your personal data.',
+                    style: TextStyle(fontSize: 13.0,
+                        color: Colors.white),
+                  ),
+                  SizedBox(height: 12.0),
+                  Text(
+                    'Information We Collect:',
+                    style: TextStyle(
+                      fontSize: 16.0,
+                      fontWeight: FontWeight.bold,
+                        color: Colors.white
+                    ),
+                  ),
+                  SizedBox(height: 8.0),
+                  Text(
+                    'When you use our app, we may collect certain personal information, such as your name, email address, and device information.\n'
+                        'We also collect information related to your use of the app, including QR codes scanned or generated, timestamps, and app settings.\n'
+                        'We may collect non-personal information such as aggregated data or statistical information about app usage patterns.',
+                    style: TextStyle(fontSize: 12.0,
+                        color: Colors.white),
+                  ),
+                  SizedBox(height: 12.0),
+                  Text(
+                    'How We Use Your Information:',
+                    style: TextStyle(
+                      fontSize: 16.0,
+                      fontWeight: FontWeight.bold,
+                        color: Colors.white
+                    ),
+                  ),
+                  SizedBox(height: 8.0),
+                  Text(
+                    'We use the collected information to provide, improve, and personalize the app experience for you.\n'
+                        'We may use your email address to send you important updates or notifications related to the app.\n'
+                        'We may use aggregated and statistical data for analytical purposes to understand app usage patterns and improve our services.',
+                    style: TextStyle(fontSize: 12.0,
+                        color: Colors.white),
+                  ),
+                  SizedBox(height: 12.0),
+                  Text(
+                    'Data Sharing and Disclosure:',
+                    style: TextStyle(
+                      fontSize: 16.0,
+                      fontWeight: FontWeight.bold,
+                        color: Colors.white
+                    ),
+                  ),
+                  SizedBox(height: 8.0),
+                  Text(
+                    'We do not sell, rent, or lease your personal information to third parties.\n'
+                        'We may share your information with trusted third-party service providers who assist us in operating and maintaining the app.\n'
+                        'We may disclose your information if required by law or to protect our rights, property, or safety.',
+                    style: TextStyle(fontSize: 12.0,
+                        color: Colors.white),
+                  ),
+                  SizedBox(height: 12.0),
+                  Text(
+                    'Security:',
+                    style: TextStyle(
+                      fontSize: 16.0,
+                      fontWeight: FontWeight.bold,
+                        color: Colors.white
+                    ),
+                  ),
+                  SizedBox(height: 8.0),
+                  Text(
+                    'We take reasonable measures to protect your personal information from unauthorized access, use, or disclosure.\n'
+                        'However, please note that no method of transmission over the internet or electronic storage is 100% secure.',
+                    style: TextStyle(fontSize: 12.0,
+                        color: Colors.white),
+                  ),
+                  SizedBox(height: 12.0),
+                  Text(
+                    'Your Choices:',
+                    style: TextStyle(
+                      fontSize: 16.0,
+                      fontWeight: FontWeight.bold,
+                        color: Colors.white
+                    ),
+                  ),
+                  SizedBox(height: 8.0),
+                  Text(
+                    'You can choose not to provide certain personal information, but it may limit your ability to use certain features of the app.\n'
+                        'You may opt out of receiving promotional emails from us by following the instructions provided in those emails.',
+                    style: TextStyle(fontSize: 12.0,
+                        color: Colors.white),
+                  ),
                 ],
               ),
             )
--- lib/ExtraPages/terms_page.dart
+++ lib/ExtraPages/terms_page.dart
@@ -30,13 +30,103 @@ class _terms_pageState extends State<terms_page> {
       ),
       body: SingleChildScrollView(
         child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
           children: [
             Container(
               margin: EdgeInsets.all(10),
               padding: EdgeInsets.all(10),
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(10),
+                color: constant.secondaryBackClr
+              ),
               child: Column(
                 children: [
-                  Text("")
+                  Text(
+                    'Welcome to Jasneh! By using our QR code scanning and QR code generator app, you agree to the following terms and conditions:',
+                    style: TextStyle(fontSize: 13.0,
+                        color: Colors.white),
+                  ),
+                  SizedBox(height: 12.0),
+                  Text(
+                    'App Usage:',
+                    style: TextStyle(
+                      fontSize: 16.0,
+                      fontWeight: FontWeight.bold,
+                        color: Colors.white
+                    ),
+                  ),
+                  SizedBox(height: 8.0),
+                  Text(
+                    'You may use our app for personal, non-commercial purposes.\n'
+                        'You agree not to use the app for any illegal or unauthorized purpose.\n'
+                        'You are responsible for maintaining the confidentiality of your account information and for all activities that occur under your account.',
+                    style: TextStyle(fontSize: 12.0,
+                        color: Colors.white),
+                  ),
+                  SizedBox(height: 12.0),
+                  Text(
+                    'Intellectual Property:',
+                    style: TextStyle(
+                      fontSize: 16.0,
+                      fontWeight: FontWeight.bold,
+                        color: Colors.white
+                    ),
+                  ),
+                  SizedBox(height: 8.0),
+                  Text(
+                    'Our app and its content, including but not limited to logos, trademarks, and designs, are the property of Jasneh and are protected by intellectual property laws.\n'
+                        'You agree not to reproduce, distribute, modify, or create derivative works based on our app without our prior written consent.',
+                    style: TextStyle(fontSize: 12.0,
+                        color: Colors.white),
+                  ),
+                  SizedBox(height: 12.0),
+                  Text(
+                    'Limitation of Liability:',
+                    style: TextStyle(
+                      fontSize: 16.0,
+                      fontWeight: FontWeight.bold,
+                        color: Colors.white
+                    ),
+                  ),
+                  SizedBox(height: 8.0),
+                  Text(
+                    'We strive to provide accurate and reliable information through our app, but we do not guarantee the accuracy, completeness, or reliability of any content.\n'
+                        'We shall not be liable for any direct, indirect, incidental, special, or consequential damages arising out of or in connection with your use of the app.',
+                    style: TextStyle(fontSize: 12.0,
+                        color: Colors.white),
+                  ),
+                  SizedBox(height: 12.0),
+                  Text(
+                    'Modifications:',
+                    style: TextStyle(
+                      fontSize: 16.0,
+                      fontWeight: FontWeight.bold,
+                        color: Colors.white
+                    ),
+                  ),
+                  SizedBox(height: 8.0),
+                  Text(
+                    'We reserve the right to modify or discontinue the app or any part thereof without prior notice.\n'
+                        'We may also update these terms and conditions from time to time, and it is your responsibility to review them periodically.',
+                    style: TextStyle(fontSize: 12.0,
+                        color: Colors.white),
+                  ),
+                  SizedBox(height: 12.0),
+                  Text(
+                    'Governing Law:',
+                    style: TextStyle(
+                      fontSize: 16.0,
+                      fontWeight: FontWeight.bold,
+                        color: Colors.white
+                    ),
+                  ),
+                  SizedBox(height: 8.0),
+                  Text(
+                    'These terms and conditions shall be governed by and construed in accordance with the laws of India.\n'
+                        'Any disputes arising out of or in connection with these terms and conditions shall be subject to the exclusive jurisdiction of the courts in India.',
+                    style: TextStyle(fontSize: 12.0,
+                        color: Colors.white),
+                  ),
                 ],
               ),
             )
